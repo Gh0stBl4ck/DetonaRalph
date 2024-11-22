4f330a3ab96168f3c8878fafb083c01485c4f884
Guilherme Lins Oliveira <166453934+Gh0stBl4ck@users.noreply.github.com>
2024-11-22 12:23:39 -0300

    Update engine.js

--- engine.js
+++ engine.js
@@ -54,7 +54,7 @@ function countDown() {
 }
 
 function playSound() {
-    let audio = new Audio("../src/audios/hit.m4a");
+    let audio = new Audio("audio/hit.m4a");
     audio.volume = 0.2;
     audio.play();
 }
